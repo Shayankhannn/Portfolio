6a0a15aad49ef28d93ec941798f8f0b115f8a13a
shayankhannn <shayannkhannn007@gmail.com>
2025-01-13 13:49:10 +0500

    portfolio

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+import { ThemeProvider } from "./Provider";
+
+
+
+export const metadata: Metadata = {
+  title: "Shayan's Portfolio",
+  description: "Shayan Khan's Portfolio",
+};
+
+export default function RootLayout({
+  children,
+} : Readonly<{
+  children : React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
