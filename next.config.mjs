27527c7ce1621d3e295f0241b009d71988e01061
shayankhannn <shayannkhannn007@gmail.com>
2025-02-22 12:51:21 +0500

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    typescript: {
+        ignoreBuildErrors: true,
+      },
+      eslint: {
+        ignoreDuringBuilds: true,
+      },
+      images: {
+        dangerouslyAllowSVG: true,
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "*",
+          }
+        ]
+      },
+};
 
 export default nextConfig;
